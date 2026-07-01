344c0f5f775fb4845156ebfb9f83126bca5845f2
sofialombardifotografia <lombardissofia@gmail.com>
2026-07-01 12:51:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
       links.classList.toggle('open');
     });
   }
+  
 });
+const dropdown = document.querySelector('.dropdown');
+
+if (dropdown) {
+  dropdown.addEventListener('click', function (e) {
+    if (window.innerWidth <= 880) {
+      e.preventDefault();
+      dropdown.classList.toggle('open');
+    }
+  });
+}
